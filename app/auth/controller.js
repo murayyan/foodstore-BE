6e77f76a3308d74d303eec5daec0881af4018799
Muhammad Rayyan <muhammad.abhad@btpn.com>
2020-12-03 22:26:40 +0700

    add function login, logout, me, and localstrategy

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,4 +1,9 @@
 const User = require("../user/model");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const config = require("../config");
+const { getToken } = require("../utils/get-token");
 
 const register = async (req, res, next) => {
   try {
@@ -23,4 +28,77 @@ const register = async (req, res, next) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res, next) => {
+  passport.authenticate("local", async (err, user) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.json({ error: 1, message: "email or password incorrect" });
+    }
+    // (1) buat JSON Web Token
+    let signed = jwt.sign(user, config.secretKey); // <--- ganti secret key dengan keymu sendiri, bebas yang sulit ditebak
+    // (2) simpan token tersebut ke user terkait
+    await User.findOneAndUpdate(
+      { _id: user._id },
+      { $push: { token: signed } },
+      { new: true }
+    );
+    // (3) response ke _client_
+    return res.json({
+      message: "logged in successfully",
+      user: user,
+      token: signed,
+    });
+  })(req, res, next);
+};
+
+const me = (req, res, next) => {
+  if (!req.user) {
+    return res.json({
+      error: 1,
+      message: `Your're not login or token expired`,
+    });
+  }
+  return res.json(req.user);
+};
+
+const logout = async (req, res, next) => {
+  // (1) dapatkan token dari `request`
+  let token = getToken(req);
+  // (2) hapus `token` dari `User`
+  let user = await User.findOneAndUpdate(
+    { token: { $in: [token] } },
+    { $pull: { token } },
+    { useFindAndModify: false }
+  );
+  // --- cek user atau token ---//
+  if (!user || !token) {
+    return res.json({
+      error: 1,
+      message: "No user found",
+    });
+  }
+  return res.json({ error: 0, message: "Logout berhasil" });
+};
+
+const localStrategy = async (email, password, done) => {
+  try {
+    // (1) cari user ke MongoDB
+    let user = await User.findOne({ email }).select(
+      "-__v -createdAt -updatedAt -cart_items -token"
+    );
+    // (2) jika user tidak ditemukan, akhiri proses login
+    if (!user) return done();
+    // (3) sampai sini artinya user ditemukan, cek password sesuai atau tidak
+    if (bcrypt.compareSync(password, user.password)) {
+      ({ password, ...userWithoutPassword } = user.toJSON());
+      // (4) akhiri pengecekkan, user berhasil login
+      // berikan data user tanpa password
+      return done(null, userWithoutPassword);
+    }
+  } catch (err) {
+    done(err, null);
+  }
+  done();
+};
+
+module.exports = { register, localStrategy, login, logout, me };
