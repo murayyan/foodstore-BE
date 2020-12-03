d7ea9ad2d8fbd212496c6a3eb561007c0e930a89
Muhammad Rayyan <muhammad.abhad@btpn.com>
2020-12-03 22:25:13 +0700

    use decode

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const { decodeToken } = require("./app/auth/middleware");
 
 const authRouter = require("./app/auth/router");
 const productRouter = require("./app/product/router");
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(decodeToken());
 app.use("/auth", authRouter);
 app.use("/api", productRouter);
 app.use("/api", categoryRouter);
